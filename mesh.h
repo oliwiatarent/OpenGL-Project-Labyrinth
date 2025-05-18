7cfef664f9544398290cae896a374ed6f29ed146
Oliwia Tarent <100858979+oliwiatarent@users.noreply.github.com>
2025-05-18 18:45:23 +0200

    Add files via upload

---
+++ mesh.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "include.h"
+#include "shaderprogram.h"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+    string path;
+};
+
+class Mesh {
+public:
+    // mesh data
+    vector<Vertex>       vertices;
+    vector<unsigned int> indices;
+    vector<Texture>      textures;
+
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+    void Draw(ShaderProgram& shader);
+private:
+    //  render data
+    unsigned int VAO, VBO, EBO;
+
+    void setupMesh();
+};
